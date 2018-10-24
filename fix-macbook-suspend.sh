6b0fcd0fdb48de10f444b9cd9df3ad8d08edd061
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2018-10-24 21:03:50 +1000

    Add hibernate sleep

--- fix-macbook-suspend.sh
+++ fix-macbook-suspend.sh
@@ -2,7 +2,10 @@
 #fix suspend on the Macbook Air
 cat > "/etc/systemd/sleep.conf" << EOL
 [Sleep]
-# Suspend to RAM
+# Suspend to RAM (S3)
 SuspendMode=suspend
 SuspendState=mem
+# Hibernate (hybrid-sleep) mem & disk
+HibernateMode=suspend
+HibernateState=disk
 EOL
