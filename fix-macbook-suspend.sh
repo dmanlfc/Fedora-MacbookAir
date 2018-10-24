5e73ae4bbbdd616b077b0718f3fa6b48a5ad2f1b
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2018-10-24 23:13:15 +1000

    check for sudo

--- fix-macbook-suspend.sh
+++ fix-macbook-suspend.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-#fix suspend on the Macbook Air
-cat > "/etc/systemd/sleep.conf" << EOL
-[Sleep]
-# Suspend to RAM (S3)
-SuspendMode=suspend
-SuspendState=mem
-# Hibernate (hybrid-sleep) mem & disk
-HibernateMode=suspend
-HibernateState=disk
-EOL
+
+if [[ $EUID -ne 0 ]]; then
+   	echo "This script must be run as root type: sudo ./installscript"
+   	exit 1
+else
+  #fix suspend on the Macbook Air
+  echo "**********************************"
+  echo "* Fixing Macbook Suspend (S3)... *"
+  echo "**********************************"
+  cat > "/etc/systemd/sleep.conf" << EOL
+  [Sleep]
+  # Suspend to RAM (S3)
+  SuspendMode=suspend
+  SuspendState=mem
+  # Hibernate (hybrid-sleep) mem & disk
+  HibernateMode=suspend
+  HibernateState=disk
+  EOL
+fi
