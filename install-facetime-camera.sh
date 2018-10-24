ff7a64755531d1c1ffbc3881290621995cb8d696
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2018-10-24 22:54:10 +1000

    formatting

--- install-facetime-camera.sh
+++ install-facetime-camera.sh
@@ -6,41 +6,41 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 else
 
-#Setup Dependancies
-echo "Ensure dependancies are installed..."
-dnf install -y make gitv curl xzcat cpio kernel-devel
+  #Setup Dependancies
+  echo "Ensure dependancies are installed..."
+  dnf install -y make gitv curl xzcat cpio kernel-devel
 
-clear
-#Install Factime Camera
-echo "**************************************************"
-echo "Installing FacetimeHD camera for $KERNELRELEASE"
-echo "**************************************************"
-cd /tmp
-git clone https://github.com/patjak/bcwc_pcie.git
-cd bcwc_pcie/firmware
-make
-make install
-cd ..
-make
-make install
-depmod
-modprobe facetimehd
-rm -rf /tmp/bcwc_pcie
+  clear
+  #Install Factime Camera
+  echo "**************************************************"
+  echo "Installing FacetimeHD camera for $KERNELRELEASE"
+  echo "**************************************************"
+  cd /tmp
+  git clone https://github.com/patjak/bcwc_pcie.git
+  cd bcwc_pcie/firmware
+  make
+  make install
+  cd ..
+  make
+  make install
+  depmod
+  modprobe facetimehd
+  rm -rf /tmp/bcwc_pcie
 
-if [ ! -d "/etc/modules-load.d" ]; then
-  echo "Creating modules-load.d directory..."
-  mkdir -p "/etc/modules-load.d"
+  #check directory exists
+  if [ ! -d "/etc/modules-load.d" ]; then
+    echo "Creating modules-load.d directory..."
+    mkdir -p "/etc/modules-load.d"
+  fi
+  
+  #check file exists
+  if [ ! -d "/etc/modules-load.d/facetimehd.conf" ]; then
+    echo "Creating FaceTimeHD config file..."
+    cat > "/etc/modules-load.d/facetimehd.conf" << EOL
+    facetimehd
+    EOL
+  fi
+  echo "**************************************"
+  echo "* FaceTime HD installation complete! *"
+  echo "**************************************"
 fi
-
-if [ ! -d "/etc/modules-load.d/facetimehd.conf" ]; then
-  echo "Creating FaceTimeHD config file..."
-  cat > "/etc/modules-load.d/facetimehd.conf" << EOL
-  facetimehd
-  EOL
-fi
-echo "**************************************"
-echo "* FaceTime HD installation complete! *"
-echo "**************************************"
-fi
-
-echo "Install complete!"
